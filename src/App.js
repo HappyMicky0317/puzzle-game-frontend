14ff60422252e91577d264a3803121388c33b5d8
happydream0317 <onedev975@gmail.com>
2023-08-09 05:34:02 +0300

    pretty code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 // import logo from './logo.svg';
-import './App.css';
-import Home from './components/home/Home';
+import "./App.css";
+import Home from "./components/home/Home";
 import Header from "./components/include/header";
-import Explaination from './components/explaination/Explaination';
-import Dice from './components/play/Dice';
-import MainPlay from './components/play/MainPlay';
-import Result from './components/play/Result';
-import Signin from './components/user/Singin';
-import Signup from './components/user/Signup';
-import MyPage from './components/user/MyPage';
-import Footer from './components/include/footer';
+import Explaination from "./components/explaination/Explaination";
+import Dice from "./components/play/Dice";
+import MainPlay from "./components/play/MainPlay";
+import Result from "./components/play/Result";
+import Signin from "./components/user/Singin";
+import Signup from "./components/user/Signup";
+import MyPage from "./components/user/MyPage";
+import Footer from "./components/include/footer";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (<div className="App">
-    <Header />
+  return (
+    <div className="App">
+      <Header />
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route path="/howtoplay" element={<Explaination/>} />
-          <Route path="/dice" element={<Dice/>} />
-          <Route path="/play/:diceResults" element={<MainPlay/>} />
-          <Route path="/result" element={<Result/>} />
-          <Route path="/user/signin" element={<Signin/>} />
-          <Route path="/user/signup" element={<Signup/>} />
-          <Route path="/user/mypage" element={<MyPage/>} />
-          <Route component={<>asdfasdf</>}/>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/howtoplay" element={<Explaination />} />
+          <Route path="/dice" element={<Dice />} />
+          <Route path="/play/:diceResults" element={<MainPlay />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/user/signin" element={<Signin />} />
+          <Route path="/user/signup" element={<Signup />} />
+          <Route path="/user/mypage" element={<MyPage />} />
+          <Route component={<>asdfasdf</>} />
         </Routes>
       </Router>
-    <Footer />
+      <Footer />
     </div>
   );
 }
